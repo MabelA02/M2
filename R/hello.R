66679c412697a833a1c333b6bdd509745b3fc4cc
MabelA02 <paolam0014@gmail.com>
2025-01-30 16:21:06 -0500

    matriz

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,48 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+# Definimos la función para operar matrices cuadradas
+operar_matrices <- function(mat1, mat2, operacion) {
+  # Verificamos que las matrices sean cuadradas y de las mismas dimensiones
+  if (!is.matrix(mat1) || !is.matrix(mat2)) {
+    stop("Ambos argumentos deben ser matrices.")
+  }
+  
+  if (nrow(mat1) != ncol(mat1) || nrow(mat2) != ncol(mat2)) {
+    stop("Ambas matrices deben ser cuadradas.")
+  }
+  
+  if (nrow(mat1) != nrow(mat2)) {
+    stop("Las matrices deben tener las mismas dimensiones.")
+  }
+  
+  # Realizamos la operación solicitada
+  switch(operacion,
+         suma = return(mat1 + mat2),
+         resta = return(mat1 - mat2),
+         multiplicacion = return(mat1 %*% mat2),
+         transposicion = return(t(mat1)),
+         stop("Operación no válida. Usa 'suma', 'resta', 'multiplicacion' o 'transposicion'.")
+  )
 }
+
+# Ejemplo de uso
+matriz1 <- matrix(1:4, nrow = 2)
+matriz2 <- matrix(5:8, nrow = 2)
+
+# Suma
+resultado_suma <- operar_matrices(matriz1, matriz2, "suma")
+print("Resultado de la suma:")
+print(resultado_suma)
+
+# Resta
+resultado_resta <- operar_matrices(matriz1, matriz2, "resta")
+print("Resultado de la resta:")
+print(resultado_resta)
+
+# Multiplicación
+resultado_multiplicacion <- operar_matrices(matriz1, matriz2, "multiplicacion")
+print("Resultado de la multiplicación:")
+print(resultado_multiplicacion)
+
+# Transposición
+resultado_transposicion <- operar_matrices(matriz1, NULL, "transposicion")
+print("Resultado de la transposición:")
+print(resultado_transposicion)
